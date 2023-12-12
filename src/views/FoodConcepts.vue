8107a49277f951f7c0cc396e1a9bd81dede1c88e
Umut <umtakpnr5@gmail.com>
2023-12-12 06:08:47 +0300

    Simple UI fixes

--- src/views/FoodConcepts.vue
+++ src/views/FoodConcepts.vue
@@ -1,37 +1,45 @@
 <template>
   <div class="container">
     <div class="row row-cols-1 row-cols-md-3 g-4">
+
       <div class="col" v-for="concept in concepts" :key="concept.id">
-        <div class="card" style="width: 600px; max-height: 600px;">
-          <img
-              :src="`/public/images/${concept.slug}.jpg`"
-              class="card-img-top"
-              alt="..."
-              style="object-fit: cover;"
-          />
-          <div class="card-body">
-            <h5 class="card-title ">{{ concept.concept }}</h5>
-            <p class="card-text description">
-                {{
-                  concept.description
-                }}
-              </p>
+
+        <RouterLink :to="`/detail/${concept.id}`">
+          <div class="card" style="width: 600px; max-height: 600px;">
+            <img
+                :src="`/public/images/${concept.slug}.jpg`"
+                class="card-img-top image-fluid"
+                alt="..."
+                style="object-fit: cover;"
+            />
+            <div class="card-body">
+              <h5 class="card-title ">{{ concept.concept }}</h5>
+              <p class="card-text description">
+                  {{
+                    concept.description
+                  }}
+                </p>
+            </div>
           </div>
-        </div>
+        </RouterLink>
+
       </div>
+
     </div>
   </div>
 </template>
 
 <script>
 import sourceData from '@/data.json'
+import { RouterLink } from 'vue-router';
 export default {
-  name: "Concept",
-  data() {
-    return {
-      concepts : sourceData.concepts,
-    }
-  },
+    name: "Concept",
+    data() {
+        return {
+            concepts: sourceData.concepts,
+        };
+    },
+    components: { RouterLink }
 };
 </script>
 
