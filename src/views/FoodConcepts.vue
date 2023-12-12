24381a6b2599ea317fe41f267737f8ba578c3ff0
Umut <umtakpnr5@gmail.com>
2023-12-12 04:41:37 +0300

    food concepts & source data combined

--- src/views/FoodConcepts.vue
+++ src/views/FoodConcepts.vue
@@ -1,70 +1,21 @@
 <template>
   <div class="container">
-    <div class="row row-cols-1 row-cols-md-2 g-4">
-      <div class="col">
-        <div class="card">
+    <div class="row row-cols-1 row-cols-md-3 g-4">
+      <div class="col" v-for="concept in concepts" :key="concept.id">
+        <div class="card" style="width: 600px; max-height: 600px;">
           <img
-              src="https://images.pexels.com/photos/1099680/pexels-photo-1099680.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+              :src="`/public/images/${concept.slug}.jpg`"
               class="card-img-top"
               alt="..."
+              style="object-fit: cover;"
           />
           <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">
-              This is a longer card with supporting text below as a natural
-              lead-in to additional content. This content is a little bit
-              longer.
-            </p>
-          </div>
-        </div>
-      </div>
-      <div class="col">
-        <div class="card">
-          <img
-              src="https://images.pexels.com/photos/1099680/pexels-photo-1099680.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
-              class="card-img-top"
-              alt="..."
-          />
-          <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">
-              This is a longer card with supporting text below as a natural
-              lead-in to additional content. This content is a little bit
-              longer.
-            </p>
-          </div>
-        </div>
-      </div>
-      <div class="col">
-        <div class="card">
-          <img
-              src="https://images.pexels.com/photos/1099680/pexels-photo-1099680.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
-              class="card-img-top"
-              alt="..."
-          />
-          <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">
-              This is a longer card with supporting text below as a natural
-              lead-in to additional content.
-            </p>
-          </div>
-        </div>
-      </div>
-      <div class="col">
-        <div class="card">
-          <img
-              src="https://images.pexels.com/photos/1099680/pexels-photo-1099680.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
-              class="card-img-top"
-              alt="..."
-          />
-          <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">
-              This is a longer card with supporting text below as a natural
-              lead-in to additional content. This content is a little bit
-              longer.
-            </p>
+            <h5 class="card-title ">{{ concept.concept }}</h5>
+            <p class="card-text description">
+                {{
+                  concept.description
+                }}
+              </p>
           </div>
         </div>
       </div>
@@ -73,14 +24,28 @@
 </template>
 
 <script>
+import sourceData from '@/data.json'
 export default {
   name: "Concept",
+  data() {
+    return {
+      concepts : sourceData.concepts,
+    }
+  },
 };
 </script>
 
 <style scoped>
 .card {
   max-width: 100%;
+}
+
+.description{
+  display: none;
+}
+
+.card:hover .description{
+  display: block;
 }
 
 .card-img-top {
