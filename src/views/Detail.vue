82b6e06438ae4384d00e0981f1724bb03d521fd7
Umut <umtakpnr5@gmail.com>
2023-12-12 06:09:11 +0300

    Detail page scheme created

--- src/views/Detail.vue
+++ src/views/Detail.vue
@@ -1,12 +1,55 @@
 <template>
-  <div>
-    <h1>Detail Page</h1>
-    <p>This is the Detail Page.</p>
+  <div
+  class="bg-image p-5 text-center shadow-1-strong mb-5 text-white"
+  :style="`background-image: url(/public/images/${selectedConcept.slug}.jpg); background-size:cover; background-position:center;`"
+>
+  <div class="d-inline-flex">
+    <h1 class="mb-3 jumbotext-bg rounded p-3" style="width: min-content;">{{ selectedConcept.concept }}</h1>
   </div>
+  <p class="lead jumbotext-bg rounded p-1">
+    {{ selectedConcept.description }}
+  </p>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
 </template>
 
 <script>
-export default {
-  name: "Detail",
-};
+  import sourceData from '@/data.json'
+  export default {
+    computed : {
+      conceptId(){
+        return parseInt(this.$route.params.id);
+      },
+      selectedConcept(){
+        return sourceData.concepts.find( concept => concept.id === this.conceptId);
+      }
+    },
+    data(){
+      return {
+        
+      }
+    }
+  };
 </script>
+
+<style>
+
+  .jumbotext-bg {
+    background-color: rgba(0, 255, 255, 0.811);
+    color: black;
+    text-decoration: black;
+  }
+
+</style>
