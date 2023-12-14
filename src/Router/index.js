bebb5c9e98b7edc3d019c7444b34442294596010
Umut <umtakpnr5@gmail.com>
2023-12-14 22:11:50 +0300

    Scroll Behavior & NotFOund regex match added

--- src/Router/index.js
+++ src/Router/index.js
@@ -7,13 +7,16 @@ const routes = [
     { path: "/detail/:id", component: ()=>import("@/views/Detail.vue") },
     { path: "/food-concepts", component: ()=>import("@/views/FoodConcepts.vue") },
     { path: "/detail/:id/:slug", component: ()=>import("@/views/RecipeDetail.vue") },
-    { path: "/404", component: ()=>import("@/views/NotFound.vue") },
+    { path: "/:pathMatch(.*)*", name: 'NotFound', component: ()=>import("@/views/NotFound.vue") },
     //{ path: "/all-recipes", component: ()=>import("@/views/RecipeDetail.vue") },
 ];
 
 const router = createRouter({
     history: createWebHistory(),
     routes,
+    scrollBehavior (to, from, savedPosition) {
+        return savedPosition || { top : 0 }
+    }
 });
 
 export default router;
